ddfb20c365bfb1912ec87c9b3c79e116446b9820
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-03-22 11:17:41 +0100

    .

--- index.php
+++ index.php
@@ -3,14 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Hackers-poulette</title>
 </head>
 <body>
-    <header></header>
+    <header>
+        <div class="container">
+            <nav class="navbar" id="navbar">
+                
+            </nav>
+        </div>
+    </header>
     <main></main>
     <footer></footer>
 </body>
