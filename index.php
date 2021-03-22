b0ab9ed27637519bff22744fe2b73a9a85ac4bda
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-03-22 16:18:17 +0100

    .

--- index.php
+++ index.php
@@ -4,26 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
 </head>
 <body>
     <header>
-        <h1 class="titre">hacker-poulette</h1>
+        <nav>
+            <div class="navbar"></div>
+        </nav>
     </header>
-    <main>
-        <section class = "contactForm">
-            <form action="" method="post">
-                <input type="text" name="fname" id="fname" placeholder="Firstname">
-                <input type="text" name="lname" id="lname" placeholder="Lastname"> <br>
-                <h2>gender: </h2>
-                <label for="gender">Man: </label><input type="radio" name="gender" id="man" value="man">
-                <label for="gender">Woman: </label><input type="radio" name="gender" id="woman" value="woman">
-            </form>
-        </section>
-    </main>
-    <footer></footer>
 </body>
-<!-- Bootstrap JS -->
-</script><script src="./assets/script/script.js"></script>
+<script src="./assets/script/script.js"></script>
 </html>
