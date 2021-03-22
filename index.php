499c07d6cfc4a04344ddd4db141522ce7789df7c
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-03-22 15:55:03 +0100

    .

--- index.php
+++ index.php
@@ -6,17 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <title>Hackers-poulette</title>
 </head>
 <body>
-    <header></header>
+    <header>
+        <h1 class="titre">hacker-poulette</h1>
+    </header>
     <main>
-        
+        <section class = "contactForm">
+            <form action="" method="post">
+                <input type="text" name="fname" id="fname" placeholder="Firstname">
+                <input type="text" name="lname" id="lname" placeholder="Lastname"> <br>
+                <h2>gender: </h2>
+                <label for="gender">Man: </label><input type="radio" name="gender" id="man" value="man">
+                <label for="gender">Woman: </label><input type="radio" name="gender" id="woman" value="woman">
+            </form>
+        </section>
     </main>
     <footer></footer>
 </body>
-<!-- Bootstrap JS-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-<script src="./assets/script/script.js"></script>
+<!-- Bootstrap JS -->
+</script><script src="./assets/script/script.js"></script>
 </html>
